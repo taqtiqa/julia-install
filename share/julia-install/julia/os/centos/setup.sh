cfde14015eb5c6ff699bc6563eba0b9e1a3a4739
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2019-11-08 19:08:40 +1100

    Add ubuntu setup and teardown scripts.

---
+++ share/julia-install/julia/os/centos/setup.sh
@@ -0,0 +1,2 @@
+${sudo} yum groupinstall "Development Tools"
+${sudo} yum install cmake gcc-gfortran libatomic m4 pkgconf-pkg-config
--- share/julia-install/julia/os/ubuntu/setup.sh
+++ share/julia-install/julia/os/ubuntu/setup.sh
@@ -1,2 +1,29 @@
 #!/usr/env/bin bash
-${sudo} apt-get install --yes build-essential cmake gfortran libatomic1 m4 perl pkg-config python wget
+# Abundance of caution.
+# Prevent Apt doing anything funky during a long build.
+systemctl stop apt-daily.timer;
+systemctl stop apt-daily-upgrade.timer;
+systemctl disable apt-daily.timer;
+systemctl disable apt-daily-upgrade.timer;
+systemctl mask apt-daily.service;
+systemctl mask apt-daily-upgrade.service;
+systemctl daemon-reload;
+
+# Disable periodic activities of apt to be safe
+cat <<EOF >/etc/apt/apt.conf.d/10periodic;
+APT::Periodic::Enable "0";
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";
+APT::Periodic::Unattended-Upgrade "0";
+EOF
+
+# Clean and nuke the package from orbit
+rm -rf /var/log/unattended-upgrades;
+apt-get -y purge unattended-upgrades;
+
+echo `ps aux | grep [a]pt`
+
+opts='--assume-yes --fix-missing --quiet --auto-remove'
+pkgs='build-essential cmake gfortran libatomic1 m4 perl pkg-config python wget'
+${sudo} apt-get install ${opts} ${pkgs}
